9faf0f93195c3300b23941da63315f314ec41b2d
nithin <nithinpedamalla12@gmail.com>
2025-03-16 18:57:37 +0530

    final-finish-2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
